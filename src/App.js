e0d9fe33d5f4e2ee66cd4bba82c169475ed6043b
abdelrahmanes <abdelrahmanelshershaby@gmail.com>
2021-10-19 21:48:51 +0200

    modify some styles

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <div className="container w-1/3 m-auto my-20 p-9 rounded-lg shadow-2xl bg-white">
+        <div className="container xsm:w-3/4  md:w-1/3 xsm:mx-auto my-20 p-9 rounded-lg shadow-2xl bg-white">
           <h2 className="text-3xl tracking-wide text-gray-700 mb-8">
             {this.state.active ? 5 : 0} birthdays today
           </h2>
@@ -22,7 +22,7 @@ class App extends Component {
           <div className="mt-10">
             <a
               href="#"
-              className="bg-pink-300 py-3 px-44 mx-auto text-white text-xl my-20"
+              className="bg-pink-500 py-4 px-44 xsm:py-2 xsm:px-16 md:py-3 md:px-20 lg:py-3 lg:px-32  mx-auto text-white text-xl my-20"
               onClick={this.clickHandel}
             >
               Clear All
